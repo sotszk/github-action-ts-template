a47c2b0cd00794d83f47422a381367b761ac9f99
SotaSuzuki <3.3sota.suzuki@gmail.com>
2020-04-27 18:16:15 +0900

    init

---
+++ __test__/index.test.ts
@@ -0,0 +1,3 @@
+test('init', () => {
+  expect(String(10)).toBe('10');
+});
---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: ['**/*.test.ts'],
+  testRunner: 'jest-circus/runner',
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  verbose: true,
+};
